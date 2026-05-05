74a01e46bbee5f5efb21b7fedee92fe37f9b684d
CarTerBr00k3 <carter.rankin@ascte.org>
2026-05-05 13:54:33 -0500

    Week 2  Tuesday Team 10

---
+++ src/types/Tweet.ts
@@ -0,0 +1,12 @@
+export type Tweet = {
+    id: number;
+    name?: string;
+    username?: string;
+    createdAt?: string;
+    text: string;
+    likes?: number;
+    replies?: number;
+    tag?: string;
+
+};
+
